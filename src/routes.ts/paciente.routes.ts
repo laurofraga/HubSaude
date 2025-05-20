b5540c8269af521bb6a0bce3768706ba9ffb00d1
laurofraga <fragalauro12@gmail.com>
2025-05-20 16:25:42 -0300

    refact: refactoring the pacient controller

--- src/routes.ts/paciente.routes.ts
+++ src/routes.ts/paciente.routes.ts
@@ -1,11 +1,12 @@
+
 import { Router } from "express";
 import { PacienteController } from "../controller/PacienteController";
 
 const router = Router();
 const controller = new PacienteController();
 
-router.get("/", controller.listarPacientes.bind);
-router.get("/:id", controller.buscarPacientePorId.bind);
+router.get("/", controller.listarPacientes);
+router.get("/:id", controller.buscarPacientePorId);
 router.post("/", controller.criarPaciente.bind);
 router.put("/:id", controller.atualizarPaciente.bind);
 router.delete("/:id", controller.deletarPaciente.bind);
