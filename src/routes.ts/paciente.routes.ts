e69ee61ea0527baccbd7fc65b18f4a75af43b065
laurofraga <fragalauro12@gmail.com>
2025-05-29 21:17:33 -0300

    fix: fixing paciente home route

--- src/routes.ts/paciente.routes.ts
+++ src/routes.ts/paciente.routes.ts
@@ -6,11 +6,11 @@ const router = Router();
 const service = new PacienteService();
 const controller = new PacienteController(service);
 
-router.get('/:id/home', controller.getHome);
-router.get("/", controller.listarPacientes);
+router.get('/home/:id', controller.getHome);
 router.get("/:id", controller.buscarPacientePorId);
 router.post("/", controller.criarPaciente);
 router.put("/:id", controller.atualizarPaciente);
 router.delete("/:id", controller.deletarPaciente);
+router.get("/", controller.listarPacientes);
 
 export default router;
